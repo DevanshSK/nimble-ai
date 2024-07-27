7b1aa50d7454af8faca13ce17160d1d74b430d9e
DevanshSK <devanshs843@gmail.com>
2024-07-28 00:31:55 +0530

    Add user login and change password schemas, sign-in form hook, and sign-in page components and finished sign in functionality.

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,10 +1,29 @@
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
 import React from 'react'
+import SignInFormProvider from '@/components/forms/sign-in/form-provider'
+import LoginForm from '@/components/forms/sign-in/login-form'
 
-type Props = {}
 
-const SignIn = (props: Props) => {
+const SignIn = () => {
   return (
-    <div>SignIn</div>
+    <div className="flex-1 py-36 md:px-16 w-full">
+      <div className="flex flex-col h-full gap-3">
+        <SignInFormProvider>
+          <div className="flex flex-col gap-3">
+            <LoginForm />
+            <div className="w-full flex flex-col gap-3 items-center">
+              <Button className="w-full" type="submit">
+                Submit
+              </Button>
+              <p>Don&apos;t have an account?{" "}
+                <Link href="/auth/sign-up" className="font-bold">Create one</Link>
+              </p>
+            </div>
+          </div>
+        </SignInFormProvider>
+      </div>
+    </div>
   )
 }
 
